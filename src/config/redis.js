34aeef3b0e16975e4cd97bfbd44932253f9682e4
ameercm988 <ameer@dopamine.fit>
2024-12-30 19:09:10 +0530

    docker_depndencies

--- src/config/redis.js
+++ src/config/redis.js
@@ -1,12 +1,15 @@
 import Redis from "ioredis";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const redisClient = new Redis({
   host: process.env.REDIS_HOST,
   port: process.env.REDIS_PORT,
-  password: process.env.REDIS_PASSWORD
+  password: process.env.REDIS_PASSWORD,
 });
 
-redisClient.on('connect', () => console.log('Connected to Redis'));
-redisClient.on('error', (err) => console.error('Redis error:', err));
+redisClient.on("connect", () => console.log("Connected to Redis"));
+redisClient.on("error", (err) => console.error("Redis error:", err));
 
 export default redisClient;
